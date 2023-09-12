146ee2cf681a9259169f0cdd065ecc1f3d470e48
katrinageorge <katrina.g027@gmail.com>
2023-09-13 00:51:45 +1000

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import Login from './pages/Login';
 
 // Change the directory the client folder and use the command 'npm start' to launch the front end on port 3000
 
@@ -21,6 +26,32 @@ function App() {
       ) : (
         data.members.map((member, i) => <p key={i}>{member}</p>)
       )}
+      <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <div>
+            <Link className="App-link" to="/">Home</Link>
+            &nbsp;|&nbsp;
+            <Link className="App-link" to="/page2">Page2</Link>
+          </div>
+          <Routes>
+            <Route exact path="/">
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </header>
+    </div>
     </div>
   );
 }
