f5e3e83d9d4a72233af3314ab2ae212c2272a6a3
katrinageorge <katrina.g027@gmail.com>
2023-09-13 15:38:19 +1000

    Completed functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,43 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HomePage from './components/Home';
-import LoginPage from './components/Login';
-//import NavBar from './components/Navbar';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-const App=()=>{
-
-    
-  return (
-    <Router>
-    <div className="">
-        {/* <NavBar/> */}
-        <Routes>
-            <Route path="/login">
-                <LoginPage/>
-            </Route>    
-            <Route path="/">
-                <HomePage/>
-            </Route>
-        </Routes>
-    </div>
-    </Router>
-  )
-}
-
-
-ReactDOM.render(<App/>,document.getElementById('root'))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
