0eddf96ba69596ff8d1accebfe28bb1ad24b0d03
katrinageorge <katrina.g027@gmail.com>
2023-09-13 15:38:19 +1000

    Completed functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,19 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import Home from './pages/home';
-import Login from './pages/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
 // Change the directory the client folder and use the command 'npm start' to launch the front end on port 3000
 
-function App() {
-  const [data, setData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("/members")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, []);
-
+const App = () => {
   return (
-    <div>
-      {typeof data.members === "undefined" ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => <p key={i}>{member}</p>)
-      )}
-      <div className="App">
-      <header className="App-header">
-        <BrowserRouter>
-          <div>
-            <Link className="App-link" to="/">Home</Link>
-            &nbsp;|&nbsp;
-            <Link className="App-link" to="/page2">Page2</Link>
-          </div>
-          <Routes>
-            <Route exact path="/">
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </header>
-    </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
