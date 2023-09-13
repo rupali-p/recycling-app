5ac00cc732bb18f4595832e576358878878db5bd
katrinageorge <katrina.g027@gmail.com>
2023-09-13 15:38:19 +1000

    Completed functionality

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HomePage from './components/Home';
-import LoginPage from './components/Login';
-//import NavBar from './components/Navbar';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
