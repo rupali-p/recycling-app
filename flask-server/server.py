2471b22bd1f1d762a1a96fbead057fb4cdfa3ecb
ClodaghMM <13559048@student.uts.edu.au>
2023-09-14 13:37:09 +1000

    Added whitespace to resolve MR on server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 @app.route("/members")
 def members():
     return {"members": ["test1", "test2", "test3"]}
+
+    
 # Runs on localhost:5000 as default
 # Change the directory to 'flask-server' and run command 'py server.py' to get started
 # Type localhost:5000/members to see
