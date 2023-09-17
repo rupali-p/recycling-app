c5af955d0dc6735785cf597b50e1e68ceea6195b
Jom-jayrome <120691210+Jom-jayrome@users.noreply.github.com>
2023-09-17 21:45:16 +1000

    Rebasing to latest main branch T042 WIP Create Login
    Work in progress wireframe to be done connecting with backend (functionality)
    
    Co-authored-by: Jasmine Bautista <jasminebautista.misc@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
@@ -13,6 +14,26 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
+=======
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Login from "./components/Login";
+
+// Change the directory the client folder and use the command 'npm start' to launch the front end on port 3000
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+>>>>>>> 87a2783 (T042 WIP Create Login)
       </Routes>
     </Router>
   );
