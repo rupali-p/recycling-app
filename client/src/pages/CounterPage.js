b8f0333648e0e6b34a6be78c4fd3c435e6c2b466
swolbean <baustria2108@gmail.com>
2023-09-17 18:53:26 +1000

    Homepage is now counter page for ease of testing.

---
+++ client/src/pages/CounterPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CounterPage = () => {
+    return (
+        <div>
+            <h1>Counter Page test test</h1>
+        </div>
+    );
+};
+
+export default CounterPage
