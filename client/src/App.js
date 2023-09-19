a52ca8e9403f2f1976e759ff2774d0500096207b
katrinageorge <katrina.g027@gmail.com>
2023-09-19 22:35:04 +1000

    had to remove to fix run errors; investigate later

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login';
 
 // Change the directory the client folder and use the command 'npm start' to launch the front end on port 3000
 
@@ -10,7 +9,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
