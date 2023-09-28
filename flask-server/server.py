8953d3050528b1ab6a00027a9432ca1ac1689c60
ClodaghMM <13559048@student.uts.edu.au>
2023-09-28 13:33:12 +1000

    Addition of dummy page to view results - currently troubleshooting

--- flask-server/server.py
+++ flask-server/server.py
@@ -12,12 +12,11 @@ def members():
 
 @app.route("/api/view-PIC")
 def view_PIC():
-    print("Retrieving PIC result")
-    #Call the function here
     result = get_pic_result("1")
     if(result != 'Error'):
         return result, 200
-    return result, 500
+    else:
+        return result, 500
 
 # Runs on localhost:5000 as default
 # Change the directory to 'flask-server' and run command 'py server.py' to get started
@@ -25,16 +24,4 @@ def view_PIC():
 
 if __name__ == "__main__":
     
-    ##Set up the mongoDB database connection
-    # uri = "mongodb+srv://SISTeam22:Torecycleornot7214@hamlet-db.kdfozrj.mongodb.net/?retryWrites=true&w=majority"
-    # # Create a new client and connect to the server
-    # client = MongoClient(uri, server_api=ServerApi('1'))
-    # # Send a ping to confirm a successful connection. This will print in the console. 
-    # try:
-    #     client.admin.command('ping')
-    #     print("Pinged your deployment. You successfully connected to MongoDB!")
-    # except Exception as e:
-    #     print(e)
-    
-    #Then run the app
     app.run(debug=True)
