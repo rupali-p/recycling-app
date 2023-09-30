827b7c564ba95865fbad6fe359e664783434a3b1
ClodaghMM <13559048@student.uts.edu.au>
2023-09-30 11:44:28 +1000

    Added login functionality
    rebased branch and resolved MR

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,8 @@
 from flask import Flask, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
 from register_user import register_user, USER_ADDED, EMAIL_EXISTS
-
+from login import login_user, LOGIN_FAILED, LOGIN_SUCCCESS
 
 app = Flask(__name__)
 
@@ -28,6 +27,25 @@ def handle_register_user():
     elif register_result == EMAIL_EXISTS:
         return res, 200
 
+@app.route("/login")
+def handle_login():
+    #Get the data from the api
+    data = request.get_json()
+    #Call the function to log the user in
+    login_result = login_user(uName = data["username"],
+    password = data["password"])
+    result = {"result": login_result}
+    
+    #Send the appropriate message
+    if login_result == LOGIN_SUCCESS:
+        return res, 200
+    elif login_result == LOGIN_FAILED:
+        return res, 401
+
+
+
+
+
 @app.route("/members")
 def members():
     return {"members": ["test1", "test2", "test3"]}
@@ -38,26 +56,6 @@ def members():
 
 if __name__ == "__main__":
     
-    ##Set up the mongoDB database connection
-    uri = "mongodb+srv://SISTeam22:Torecycleornot7214@hamlet-db.kdfozrj.mongodb.net/?retryWrites=true&w=majority"
-    # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
-    #Connect up to the user's collection. 
-    db = client["Hamlet-DB"]
-    user_col = db['Users']
     
-    #Login, test user 
-    userName = 'testUser'
-    password = 'secret'
-
-    print("Checking credentials")
-    document = user_col.find_one({"username" : userName})
-    try:
-        if(userName == document.get("username") and password == document.get("password")):
-            print("User matches")
-            print("Password matches")
-            print("Login")
-    except Exception:
-        print("Incorrect credentials. Please try again")
     #Then run the app
     app.run(debug=True)
