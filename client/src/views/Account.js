967200ca00fae2564c4045fa91b0f967742fd780
Jom-jayrome <120691210+Jom-jayrome@users.noreply.github.com>
2023-10-08 20:55:32 +1100

    Update Account Styling
    Account Page styling (only frontend, no functionality)
    
    No work on counter page yet

--- client/src/views/Account.js
+++ client/src/views/Account.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
 import moebius from "../images/moebius-loop.png";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+import { Button } from '@mui/material'
 
 const councils = [
   {
@@ -25,102 +21,76 @@ const councils = [
   },
 ];
 
+const tabStyle = {
+  margin: '10px', 
+  color: 'black', 
+  minWidth: '40rem',
+  backgroundColor: '#d9d9d9',
+  borderRadius: '10px',
+};
+
 const Account = () => {
   const [council, setCouncil] = React.useState('');
+  const [tabValue, setTabValue] = React.useState(0);
 
-  const handleChange = (event) => {
-    setCouncil(event.target.value);
+  const handleChange = (event, newValue) => {
+    setTabValue(newValue);
   };
+
   return (
     <div
       style={{
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'column',
+        // Align children components to the start of the column
         height: '100vh', // Set the height of the container to the full viewport height
         background: 'linear-gradient(to right, #12271e, #1a4d39)',
         position: 'relative', // Add position relative to allow z-index to work
+        padding: '20px', // Add some padding for better spacing
       }}
     >
-      <div className="moebius-image-container" style={{ position: 'absolute', zIndex: 0, left: -5 }}>
-        <img src={moebius} alt="Moebius Loop" style={{ paddingTop: 15 }} />
-      </div>
-      <Card sx={{ maxWidth: 300, zIndex: 1 }}>
-        <div
+      <Typography variant="h2" style={{ marginBottom: '20px', color: 'white', fontSize: '8rem' }}>
+        Hamlet.
+      </Typography>
+      <Tabs value={tabValue} onChange={handleChange}>
+        <Tab label="Tab 1"
           style={{
-            backgroundColor: '#1a4d39', // Green color for the header
-            padding: '16px',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            borderTopLeftRadius: '8px',
-            borderTopRightRadius: '8px',
-          }}
-        >
-          <Typography variant="h5" component="div" sx={{ color: 'white' }}>
-            Account Details
-          </Typography>
-          <div>
-            <IconButton color="primary" aria-label="Edit">
-              <EditIcon />
-            </IconButton>
-            <IconButton color="error" aria-label="Delete">
-              <DeleteIcon />
-            </IconButton>
-          </div>
+            color: 'white',
+            minWidth: '20rem'
+          }} />
+        <Tab label="Tab 2"
+          style={{
+            color: 'white',
+            minWidth: '20rem'
+          }} />
+      </Tabs>
+      {tabValue === 0 && (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
+          <TextField label="Name" variant="outlined" style={tabStyle} InputLabelProps={{ style: { color: 'black' } }}/>
+          <TextField label="Email" variant="outlined" style={tabStyle} InputLabelProps={{ style: { color: 'black' } }} />
+          <TextField label="Password" variant="outlined" style={tabStyle} InputLabelProps={{ style: { color: 'black' } }} />
+          <TextField label="Postcode" variant="outlined" style={tabStyle} InputLabelProps={{ style: { color: 'black' } }} />
+          <Button style={{
+            borderRadius: '50px',
+            textTransform: 'none',
+            padding: '10px 20px',
+            borderColor: 'blue',
+            backgroundColor: '#d9d9d9',
+            color: 'black',
+            margin: '10px',
+            minWidth: '30rem'
+          }}>Edit</Button>
+          <Button> Sign Out </Button>
         </div>
-        <CardContent>
-          <TextField
-            fullWidth
-            label="First Name"
-            variant="outlined"
-            margin="normal"
-            value="Jerome" // Hard coded name value
-            InputProps={{ readOnly: true }} // Making the field read-only
-          />
-          <TextField
-            fullWidth
-            label="Last Name"
-            variant="outlined"
-            margin="normal"
-            value="Sario" // Hard coded age value
-            InputProps={{ readOnly: true }} // Making the field read-only
-          />
-          <TextField
-            select
-            fullWidth
-            label="Council"
-            variant="outlined"
-            margin="normal"
-            value={council}
-            onChange={handleChange}
-          >
-            {councils.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-
-          <TextField
-            fullWidth
-            label="Email"
-            variant="outlined"
-            margin="normal"
-            value="jerome@gmail.com" // Hard coded email value
-            InputProps={{ readOnly: true }} // Making the field read-only
-          />
-          <Button
-            variant="contained"
-            fullWidth
-            color="primary"
-            style={{ marginTop: '20px', width: '100%', justifyContent: 'center', borderRadius: '8px' }}
-          >
-            Logout
-          </Button>
-        </CardContent>
-      </Card>
-
+      )}
+      {tabValue === 1 && (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <TextField label="Name" variant="outlined" style={{ margin: '10px', color: 'white' }} InputLabelProps={{ style: { color: 'black' } }} />
+          <TextField label="Emaissssssl" variant="outlined" style={{ margin: '10px', color: 'white' }} InputLabelProps={{ style: { color: 'white' } }} />
+          <TextField label="Password" variant="outlined" style={{ margin: '10px', color: 'white' }} InputLabelProps={{ style: { color: 'white' } }} />
+          <TextField label="Postcode" variant="outlined" style={{ margin: '10px', color: 'white' }} InputLabelProps={{ style: { color: 'white' } }} />
+        </div>
+      )}
     </div>
   )
 };
