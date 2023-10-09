6c0bf7028792250eb4326df47abac501ea165913
tam-debug <tyylow@gmail.com>
2023-10-09 21:56:28 +1100

    Make input images squares and implement mobile photo capture.

--- client/src/views/ScanImage.js
+++ client/src/views/ScanImage.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import TakePicture from "../components/Camera";
 import Button from "@mui/material/Button";
 import FileUploadIcon from '@mui/icons-material/FileUpload';
-
-// Change the directory the client folder and use the command 'npm start' to launch the front end on port 3000
 
 const ScanImage = () => {
     const [image, setImage] = useState();
@@ -41,8 +39,6 @@ const ScanImage = () => {
             Upload();
         } else {
             UploadCapturedImage();
-            // formData = new FormData();
-            // formData.append("image", inputImage)
         }
 
 
@@ -50,15 +46,43 @@ const ScanImage = () => {
 
     const handleSave = (imageDataUrl) => {
         console.log("Got image")
-        // console.log(imageDataUrl)
-        // const file = new File([imageBlob], "input_image",{ type: "image/png" })
         setInputImage(imageDataUrl);
         setIsImageFromFile(false);
     }
 
+    const handleUploadFile = async (event) => {
+
+        const reader = new FileReader()
+        reader.onloadend = () => {
+            var imageFile = new Image();
+            imageFile.src = reader.result
+            imageFile.onload = () => {
+                const canvas = document.getElementById("image-preview")
+                canvas.width = 400
+                canvas.height = 400
+                var imgSize = Math.min(imageFile.width, imageFile.height);
+                var left = (imageFile.width - imgSize) / 2;
+                var top = (imageFile.height - imgSize) / 2;
+                var contex = canvas.getContext("2d");
+
+                contex.drawImage(imageFile, left, top, imgSize, imgSize, 0, 0, canvas.width, canvas.height)
+                setInputImage(canvas.toDataURL().split(';base64,')[1])
+                setIsImageFromFile(false);
+
+            }
+
+            console.log("Image File")
+            console.log(imageFile)
+
+        }
+        reader.readAsDataURL(event.target.files[0])
+    }
+
+
     return(
         <>
-            <TakePicture handleSave={handleSave}/>
+            <canvas id={"image-preview"}/>
+            <TakePicture handleTakePhoto={handleSave} />
             <form onSubmit={handleSubmit} className="container mt-5 pt-5 pb-5" encType="multipart/form-data">
                 <div className="form-inline justify-content-center mt-5">
                     {/*<label htmlFor="image" className="ml-sm-4 font-weight-bold mr-md-4">Image : </label>*/}
@@ -70,11 +94,7 @@ const ScanImage = () => {
                             accept="image/*"
                             className="file-custom"
                             hidden="true"
-                            onChange={(event) => {
-                                console.log(event.target.files[0]);
-                                setInputImage(event.target.files[0]);
-                                setIsImageFromFile(true);
-                            }}
+                            onChange={handleUploadFile}
                         />
                         <label htmlFor="image">
                           <Button component="span">
