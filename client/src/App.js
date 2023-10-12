5a6992f8cc6c426eb7e3882b3ae25348cfab9478
Beatrix Austria <62579267+swolbean@users.noreply.github.com>
2023-10-13 10:34:14 +1100

    Counter Page Creation (#21)
    * Added Counter Page Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
 import SignUp from './views/SignUp';
+import Counter from "./views/Counter";
 import ScanImage from './views/ScanImage';
 import UploadImage from './views/UploadImage';
 
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/SignUp" element={<SignUp />}/>
         <Route path="/ScanImage" element={<ScanImage/>}/>
         <Route path="/UploadImage" element={<UploadImage/>}/>
+          <Route path="/Counter" element={<Counter />} />
       </Routes>
     </Router>
   );
