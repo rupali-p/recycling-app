3aa9ebd207677f3377a003c7531dd225367f3770
tam-debug <tyylow@gmail.com>
2023-10-13 10:39:35 +1100

    Resolved PIC result import

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Login';
 import SignUp from './views/SignUp';
-import PICResult from './views/PICResult';
 import Counter from "./views/Counter";
 import ScanImage from './views/ScanImage';
 import UploadImage from './views/UploadImage';
