f10b480653000f72c4d19d06e41d648b9e445af3
katrinageorge <katrina.g027@gmail.com>
2023-10-17 00:01:18 +1100

    Wipe newly added pages

--- client/src/views/AboutUs.js
+++ client/src/views/AboutUs.js
@@ -9,58 +9,15 @@ import { Navbar } from "../components/Navbar";
 
 import { Link } from "react-router-dom";
 
-const BeginScan = () => {
+const AboutUs = () => {
   return (
     <div>
       <div className="gradient_background">
         <Navbar />
-        <div className="bodyWrapper">
-          <div className="descriptive-text">
-            <p sx={{ p: 5 }}>
-              THIS IS ACTUALLY ABOUT US HAHAHAHA
-              <br />
-              You can do better than that.
-              <br />
-            </p>
-            <p>
-              <Button
-                variant="contained"
-                color="grey"
-                sx={{ p: 2 }}
-                href={"./ScanImage"}
-              >
-                Live Scan
-              </Button>
-              <Button
-                variant="contained"
-                color="grey"
-                sx={{ p: 2 }}
-                href={"./UploadImage"}
-              >
-                Upload Scan
-              </Button>
-              <Button variant="contained" color="grey" sx={{ p: 2 }}>
-                <Link to="/Login">Login</Link>
-              </Button>
-              <Button variant="contained" color="grey" sx={{ p: 2 }}>
-                <Link to="/SignUp">Sign Up</Link>
-              </Button>
-              <Button variant="contained" color="grey" sx={{ p: 2 }}>
-                <Link to="/Login">Login</Link>
-              </Button>
-              <Button variant="contained" color="grey" sx={{ p: 2 }}>
-                <Link to="/Account">Account</Link>
-              </Button>
-            </p>
-          </div>
-
-          <div className="moebius-image-container">
-            <img src={moebius} alt="Moebius Loop" style={{ paddingTop: 15 }} />
-          </div>
-        </div>
+        <a>Chuck some about us-y stuff here xx</a>
       </div>
     </div>
   );
 };
 
-export default BeginScan;
+export default AboutUs;
