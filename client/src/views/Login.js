c70f89da3ddabcf34d9d6a2679d2e2e77d422a16
katrinageorge <katrina.g027@gmail.com>
2023-10-18 00:17:59 +1100

    trying to stop page refreshing

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,33 +1,53 @@
-import * as React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { setAuthToken } from '../Auth'; // Import the auth utility
-import axios from 'axios'; // Import axios
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { setAuthToken } from '../Auth';
+import axios from 'axios';
 
 const defaultTheme = createTheme();
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginResult, setLoginResult] = useState("");
-  const [loginResultSeverity, setLoginResultSeverity] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginResult, setLoginResult] = useState('');
+  const [loginResultSeverity, setLoginResultSeverity] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    await loginUser();
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Check if the form is valid before attempting to log in
+    if (validateForm()) {
+      try {
+        await loginUser();
+      } catch (error) {
+        console.error('Error during login:', error);
+      }
+    }
+  };
+
+  const validateForm = () => {
+    // Basic validation: Check if email and password are not empty
+    if (email.trim() === '' || password.trim() === '') {
+      setLoginResultSeverity('error');
+      setLoginResult('Please enter both email and password.');
+      return false; // Form is not valid
+    }
+
+    // Additional validation logic if needed
+
+    return true; // Form is valid
   };
 
   const loginUser = async () => {
@@ -41,26 +61,26 @@ const Login = () => {
 
       if (result === 'success') {
         setAuthToken(access_token);
-        setLoginResultSeverity("success");
+        setLoginResultSeverity('success');
         setLoginResult(`${result} Redirecting to Home page...`);
         setTimeout(() => {
-          navigate("/Account");
+          navigate('/Account');
         }, 2000);
       } else if (result === 'failed') {
-        setLoginResultSeverity("error");
-        setLoginResult("Login error: Invalid username or password");
+        setLoginResultSeverity('error');
+        setLoginResult('Login error: Invalid username or password');
       }
     } catch (error) {
       console.error('Login error:', error);
-      setLoginResultSeverity("error");
-      setLoginResult("Login error: Something went wrong");
+      setLoginResultSeverity('error');
+      setLoginResult('Login error: Something went wrong');
     }
   };
 
   return (
     <form onSubmit={handleSubmit} action={<Link to="/" />}>
       <ThemeProvider theme={defaultTheme}>
-        <Grid container component="main" sx={{ height: "100vh" }}>
+        <Grid container component="main" sx={{ height: '100vh' }}>
           <CssBaseline />
           <Grid
             item
@@ -69,14 +89,14 @@ const Login = () => {
             md={7}
             sx={{
               backgroundImage:
-                "url(https://source.unsplash.com/random?wallpapers)",
-              backgroundRepeat: "no-repeat",
+                'url(https://source.unsplash.com/random?wallpapers)',
+              backgroundRepeat: 'no-repeat',
               backgroundColor: (t) =>
-                t.palette.mode === "light"
+                t.palette.mode === 'light'
                   ? t.palette.grey[50]
                   : t.palette.grey[900],
-              backgroundSize: "cover",
-              backgroundPosition: "center",
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
           />
           <Grid
@@ -92,12 +112,12 @@ const Login = () => {
               sx={{
                 my: 8,
                 mx: 4,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
               }}
             >
-              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
               <Typography component="h1" variant="h5">
                 Sign in
               </Typography>
@@ -135,7 +155,7 @@ const Login = () => {
                   fullWidth
                   variant="contained"
                   sx={{ mt: 3, mb: 2 }}
-                  onClick={loginUser}
+                  // onClick={loginUser}
                 >
                   Sign In
                 </Button>
