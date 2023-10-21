c3d2d0acbf6f305534a0da301faa228153b7ac81
swolbean <baustria2108@gmail.com>
2023-10-21 14:21:50 +1100

    Can add markers to the map.

--- client/src/views/MapTest.js
+++ client/src/views/MapTest.js
@@ -1,28 +1,39 @@
-import * as React from "react";
+
 import "../css/Common.css";
 
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
 
 
 const MapTest = () => {
+    const google = window.google;
+
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: "AIzaSyDxJ60iJES2xkvAYCY1NuJyLQs5RqBONSo",
     });
-    const center = useMemo(() => ({ lat: -33.8688, lng: 151.2093 }), []);
 
-    
+    const markers = [
+        { lat: -33.8688, lng: 151.2093 }
+    ];
+
+    const onLoad = (map) => {
+        const bounds = new google.maps.LatLngBounds();
+        markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+        map.fitBounds(bounds);
+    };
 
     return (
-        <div className="App">
+        <div >
             {!isLoaded ? (
             <h1>Loading...</h1>
             ) : (
-            <GoogleMap
-                mapContainerClassName="map-container"
-                center={center}
-                zoom={10}
-            />
+                <GoogleMap mapContainerClassName="map-container" 
+                                onLoad={onLoad}  
+                                zoom={15}
+                                center={{ lat: -33.8688, lng: 151.2093 }}>
+                {markers.map(({ lat, lng }) => (
+                  <Marker position={{ lat, lng }} />
+                ))}
+              </GoogleMap>
             )}
         </div>
     );
