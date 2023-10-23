9b5a820e7a6b919824e15d89f104059980f114d0
Jom-jayrome <120691210+Jom-jayrome@users.noreply.github.com>
2023-10-24 09:18:18 +1100

    Fixing Merge Issues

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,23 +1,3 @@
-<<<<<<< Updated upstream
-import * as React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Snackbar from "@mui/material/Snackbar";
-import IconButton from "@mui/material/IconButton";
-import MuiAlert from "@mui/material/Alert";
-=======
 import * as React from 'react';
 import {useState} from 'react';
 import {useNavigate} from "react-router-dom";
@@ -36,7 +16,9 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import hamlet from "../images/hamlet-only.png";
 import "../css/Common.css";
 import nature from "../images/nature.avif";
->>>>>>> Stashed changes
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import MuiAlert from "@mui/material/Alert";
 
 const defaultTheme = createTheme();
 
@@ -119,7 +101,6 @@ const Login = () => {
     });
   };
 
-<<<<<<< Updated upstream
   return (
     <ThemeProvider theme={defaultTheme}>
       <Snackbar
@@ -137,21 +118,6 @@ const Login = () => {
         </Alert>
       </Snackbar>
       <Grid container component="main" sx={{ height: "100vh" }}>
-=======
-
-          } else if (result.status === 401) {
-            setLoginResultSeverity('Login error');
-              setLoginResult(data.result);
-          }
-      })
-  })
-  }
-
-    return (
-      <form onSubmit={handleSubmit} action={<Link to="/"/>}>
-      <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main">
->>>>>>> Stashed changes
         <CssBaseline />
         <Grid
           item
@@ -159,22 +125,10 @@ const Login = () => {
           sm={4}
           md={7}
           sx={{
-<<<<<<< Updated upstream
-            backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-=======
             backgroundImage: `url(${nature})`,
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
->>>>>>> Stashed changes
           }}
         />
         <Grid item className="gradient_background" xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -187,11 +141,6 @@ const Login = () => {
               alignItems: "center",
             }}
           >
-<<<<<<< Updated upstream
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-=======
               <img src={hamlet} alt="Hamlet" style={{
       margin: '8px',
       width: '40vh', // Default width for phone size
@@ -200,7 +149,6 @@ const Login = () => {
     }} className="hamlet-image" />
             <Typography component="h2" variant="h5" style={{ color: 'white' }}>
               Login
->>>>>>> Stashed changes
             </Typography>
             <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
@@ -272,8 +220,4 @@ const Login = () => {
   );
 };
 
-<<<<<<< Updated upstream
 export default Login;
-=======
-  export default Login;
->>>>>>> Stashed changes
