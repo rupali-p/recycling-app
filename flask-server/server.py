0b08416b26652e1524934708b52de4fdda2c6711
Beatrix Austria <62579267+swolbean@users.noreply.github.com>
2023-10-29 20:53:19 +1100

    Database GET requests for account details  (#44)
    * populated account details from the database. Modified front-end of account page for mobile compatibility. 
    
    ---------
    
    Co-authored-by: ClodaghMM <13559048@student.uts.edu.au>
    Co-authored-by: tam-debug <tyylow@gmail.com>

--- flask-server/server.py
+++ flask-server/server.py
@@ -12,11 +12,17 @@ from models.ml_model import make_prediction
 from register_user import register_user, USER_ADDED, EMAIL_EXISTS
 from get_scan_results import get_recycling_result, get_recycling_results
 from login import login_user, LOGIN_FAILED, LOGIN_SUCCCESS
+from account_details import (
+    get_all_account_details,
+    get_account_name_details,
+    get_account_password_details,
+    get_account_postcode_details,
+    get_account_scancount_details,
+)
 import json
 import numpy as np
 
 from const import OUTPUT_IMAGE_FORMAT
-
 
 app = Flask(__name__)
 
@@ -36,6 +42,7 @@ def handle_register_user():
         first_name=data["firstName"],
         last_name=data["lastName"],
         email=data["email"],
+        postcode=data["postcode"],
     )
     res = {"result": register_result}
     print(register_result)
@@ -45,9 +52,11 @@ def handle_register_user():
     elif register_result == EMAIL_EXISTS:
         return res, 200
 
+
 def np_encoder(object):
     if isinstance(object, np.generic):
         return object.item()
+
 
 # TODO: Return the classification in the response
 @app.route("/api/upload", methods=["POST"])
@@ -66,7 +75,9 @@ def handle_upload():
     decoded = base64.b64decode(image_data)
     buf = io.BytesIO(decoded)
 
-    results, model_used = make_prediction(input_image=buf, output_img_format=OUTPUT_IMAGE_FORMAT)
+    results, model_used = make_prediction(
+        input_image=buf, output_img_format=OUTPUT_IMAGE_FORMAT
+    )
     results_image = results.get("results_image")
     image_format = results.get("image_format")
     detections = results.get("detections")
@@ -78,21 +89,21 @@ def handle_upload():
             "image": results_image,
             "encoding": image_format.lower(),
             "detections": json.dumps(detections, default=np_encoder),
-            "model_used": model_used
+            "model_used": model_used,
         }
     )
 
+
 @app.route("/Login", methods=["POST"])
 def handle_login():
-    #Get the data from the api
+    # Get the data from the api
     data = request.get_json()
-    #Call the function to log the user in
+    # Call the function to log the user in
     print("In backend")
-    login_result = login_user(uName = data["Usernane"],
-    password = data["password"])
+    login_result = login_user(uName=data["Usernane"], password=data["password"])
     result = {"result": login_result}
 
-    #Send the appropriate message
+    # Send the appropriate message
     if login_result == LOGIN_SUCCCESS:
         print("Logged in, sending results")
         return result, 200
@@ -100,20 +111,51 @@ def handle_login():
         return result, 401
 
 
+@app.route("/api/get-accountdetails/<email>", methods=["GET"])
+def get_account_email(email):
+    account_details = get_all_account_details(email)
+    return account_details, 200
 
+
+@app.route("/api/get-accountdetails-name/<email>")
+def get_account_name(email):
+    account_name = get_account_name_details(email)
+    return account_name, 200
+
+
+@app.route("/api/get-accountdetails-postcode/<email>")
+def get_account_postcode(email):
+    account_postcode = get_account_postcode_details(email)
+    return account_postcode
+
+
+@app.route("/api/get-accountdetails-password/<email>")
+def get_account_password(email):
+    account_password = get_account_password_details(email)
+    return account_password
+
+
+@app.route("/api/get-accountdetails-scancount/<email>")
+def get_account_scancount(email):
+    account_scancount = get_account_scancount_details(email)
+    return account_scancount
 
 
 @app.route("/members")
 def members():
     return {"members": ["test1", "test2", "test3"]}
 
+
 @app.route("/api/view-result/<articleNum>")
 def view_result(articleNum):
-    result = get_recycling_result(articleNum) #This number should be changed to an input passed from the machine when need be
-    if(result != 'Error'):
+    result = get_recycling_result(
+        articleNum
+    )  # This number should be changed to an input passed from the machine when need be
+    if result != "Error":
         return result, 200
     else:
         return result, 500
+
 
 @app.route("/api/view-results", methods=["POST"])
 def view_results():
@@ -122,14 +164,7 @@ def view_results():
     print(article_numbers)
     arl_results = get_recycling_results(article_numbers)
     print(arl_results)
-    return jsonify(
-        {
-            "success": True,
-            "arl_results": json.dumps(arl_results)
-        }
-    )
-
-
+    return jsonify({"success": True, "arl_results": json.dumps(arl_results)})
 
 
 # Runs on localhost:5000 as default
@@ -137,5 +172,4 @@ def view_results():
 # Type localhost:5000/members to see
 
 if __name__ == "__main__":
-
     app.run(debug=True)
